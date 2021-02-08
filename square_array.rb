7c85e0fad488cddbb64fe1ae5fdde78b9ae8d930
rupinderkaur30-gif <rupi063094@gmail.com>
2021-02-08 08:48:03 -0500

    commit tree 34c31f5dfac1ec0249ac47afd1d40a620576faf4

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
-end
+
+  numbers = [1,2,3]
+
+ def square_array(numbers)
+  new_numbers=[]
+  numbers.each do |numbers|
+    new_numbers<<(numbers**2)
+  end
+  return new_numbers
+  
+  end
